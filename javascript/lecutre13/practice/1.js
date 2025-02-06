e4bd5af401aa6336fcebe54ff071baf36aaa66d6
dushyantisbest <dushyant19072003@gmail.com>
2025-02-06 21:39:46 +0530

    add practice of js lecture 13

---
+++ javascript/lecutre13/practice/1.js
@@ -0,0 +1,13 @@
+const light = "red";
+if (light === "red") {
+
+    console.log(`light is ${light} . please stop`)  
+} else if (light === "yellow") {
+    console.log(`light is ${light}. please wait`)
+}
+else if (light === "green"){
+    console.log(`light is ${light}. please go`)
+}
+else{
+    console.log("enter valid input")
+}
---
+++ javascript/lecutre13/practice/3.js
@@ -0,0 +1,7 @@
+let str = "akkkkkke";
+
+if (str[0] === "a" && str.length > 3) {
+  console.log("This is a good string");
+} else {
+  console.log("Not a good string");
+}
