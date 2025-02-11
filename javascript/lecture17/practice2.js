0383ba293c4a22ec87e8a1a6fa217d0ff7a43d0b
dushyantisbest <dushyant19072003@gmail.com>
2025-02-11 09:24:51 +0530

    add p1 and p1 (lecture17) js

---
+++ javascript/lecture17/practice2.js
@@ -0,0 +1,29 @@
+let str = "abcdabcdefgggh";
+
+function uniqueChar(str) {
+  let dict = {};
+  for (let i = 0; i < str.length; i++) {
+    if (dict[str[i]] == undefined) {
+      dict[str[i]] = 1;
+    } else {
+      dict[str[i]] += 1;
+    }
+  }
+//   console.log(Object.keys(dict));
+    let ans = "";
+    for(key in dict){
+        ans += key;
+    }
+    console.log(ans);
+}
+
+
+function uniqueChar2(str ) {
+    let arr = [];
+    for(let i=0; i < str.length; i++){
+        for (let j = 0; j < str.length; j++) {
+            const element = str[j];
+            
+        }
+    }
+}
