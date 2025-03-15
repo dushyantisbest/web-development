7c75ef5292db5ae191088ba5ce0cca1947bcefcf
dushyantisbest <dushyant19072003@gmail.com>
2025-03-15 15:05:09 +0530

    add lecture 19

---
+++ javascript/lecture20/Q1/1.js
@@ -0,0 +1,6 @@
+let input = document.createElement("input");
+let button = document.createElement("button");
+// let btn = document.getElementsByTagName("button");
+btn.innerHTML = "click me";
+
+input.appendChild
