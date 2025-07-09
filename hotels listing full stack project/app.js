67c75bb288fae0e3d4781c727322444bf59ac6cd
dushyantisbest <dushyant19072003@gmail.com>
2025-07-09 21:45:23 +0530

    added routes

--- hotels listing full stack project/app.js
+++ hotels listing full stack project/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import methodOverride from "method-override";
 
 //get the dirname variable
 const __filename = fileURLToPath(import.meta.url);
@@ -14,4 +15,6 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
 export default app;
--- hotels listing full stack project/index.js
+++ hotels listing full stack project/index.js
@@ -1,13 +1,58 @@
 import app from "./app.js";
 import connectionInstance from "./db.js";
-import InputData from "./init.js";
+import Listing from "./models/listing.model.js";
+// import InputData from "./init.js";
 await connectionInstance();
-
-
 //run to imput fake data
 // InputData();
 
+//routes
 
+//read
+app.get("/listing", async (req, res) => {
+  const list = await Listing.find({});
+  res.render("landing.ejs", { list });
+});
+
+//create
+app.get("/listing/add", (req, res) => {
+  res.render("form.ejs");
+});
+
+app.post("/listing/add", async (req, res) => {
+  await Listing.insertOne(req.body);
+  res.redirect("/listing");
+});
+
+// to display indivisul hotel data
+app.get("/listing/:id", async (req, res) => {
+  const { id } = req.params;
+  const hotelData = await Listing.findById(id);
+  res.render("indivisual_description.ejs", { hotelData });
+});
+
+//edit
+app.get("/listing/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  let hotelData = await Listing.findById(id);
+
+  res.render("edit-form.ejs", { hotelData });
+});
+
+//update
+app.put("/listing/edit/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await Listing.findByIdAndUpdate(id, req.body);
+  res.redirect(`/listing/${id}`);
+});
+
+//delete
+app.delete("/listing/delete/:id", async (req, res) => {
+  let id = req.params.id;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listing");
+});
 
 app.listen(8080, () => {
   console.log("server is working");
