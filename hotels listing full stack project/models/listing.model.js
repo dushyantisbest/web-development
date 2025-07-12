533428f857f1ab2437347556f6527d9655da0880
dushyantisbest <dushyant19072003@gmail.com>
2025-07-12 23:28:53 +0530

    styled home page

--- hotels listing full stack project/models/listing.model.js
+++ hotels listing full stack project/models/listing.model.js
@@ -13,7 +13,7 @@ const listingSchema = new mongoose.Schema(
           ? "https://www.google.com/imgres?q=random%20aesthetic%20pictures&imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2F80%2F39%2F01%2F803901270a642646222fa6a6a1737ac8.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Frandom-aesthetic--581597739391014360%2F&docid=1X4g8okeH29i_M&tbnid=-02o_Yhg-0MhkM&w=675&h=1200&hcb=2"
           : value,
     },
-    price: { type: String, required: true },
+    price: { type: Number, required: true },
     location: { type: String, required: true },
     country: { type: String, required: true },
     description: { type: String, required: true },
