f5729bf83e8c3da4091551a63111c4781bd6ffd2
dushyantisbest <dushyant19072003@gmail.com>
2025-08-19 18:42:52 +0530

    user model created

---
+++ hotels listing full stack project/models/user.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import passportLocalMongoose from "passport-local-mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    email: { type: String, required: true },
+  },
+  { timestamp: true }
+);
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
